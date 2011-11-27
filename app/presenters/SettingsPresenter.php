e9c7a4a461dc529e94159d409d1139d3ba86128d
Jakub Kinst <jakub@kinst.cz>
2011-11-27 23:33:06 +0100

    bugfix
    Signed-off-by: Jakub Kinst <jakub@kinst.cz>

--- app/presenters/SettingsPresenter.php
+++ app/presenters/SettingsPresenter.php
@@ -27,6 +27,8 @@ class SettingsPresenter extends MasterPresenter {
         $form->addCheckbox('showEmail', 'Show my e-mail address to my coursemates');
 	$langs =array('cs' => _('Czech'),'en' => _('English'));
 	$form->addSelect('lang', 'Preffered language', $langs);
+	$days = array(1=>'1',2=>'2',3=>'3',4=>'4',5=>'5',6=>'6',7=>'7',8=>'8',9=>'9',10=>'10');
+	$form->addSelect('assignment_notif_interval', 'Notify X days before assignment duedate', $days);
         $form->addSubmit('post', 'Save');    
         $form->onSubmit[] = callback($this, 'SettingsFormSubmitted');
         return $form;
