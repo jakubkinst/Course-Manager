2c9fac0600a683f5604966a94008f82c9435abcf
Jakub Kinst <jakub@kinst.cz>
2011-12-12 16:38:27 +0100

    a

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -6,7 +6,7 @@
  * @author     Jakub Kinst <jakub@kinst.cz> (@link http://jakub.kinst.cz)
  * @package    Course-Manager/Presenters/Tools
  */
-class ErrorPresenter extends BaseCoursePresenter {
+class ErrorPresenter extends BasePresenter {
 
     /**
      * Error homepage
@@ -14,7 +14,16 @@ class ErrorPresenter extends BaseCoursePresenter {
      * @return void
      */
     public function renderDefault($exception) {
-	$this->setView('500');
+	if ($this->isAjax()) { // AJAX request? Just note this error in payload.
+	    $this->payload->error = TRUE;
+	    $this->terminate();
+	} elseif ($exception instanceof BadRequestException) {
+	    $code = $exception->getCode();
+	    $this->setView(in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx'); // load template 403.latte or 404.latte or ... 4xx.latte
+	} else {
+	    $this->setView('500'); // load template 500.latte
+	    Debug::log($exception, Debug::ERROR); // and log exception
+	}
     }
 
 }
