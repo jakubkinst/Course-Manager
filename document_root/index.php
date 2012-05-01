bc9db7be3f021996660fd7c25fde59031aaa05e2
Jakub Kinst <jakub@kinst.cz>
2012-05-01 23:19:31 +0200

    (WEB) bug fix

--- document_root/index.php
+++ document_root/index.php
@@ -7,7 +7,7 @@
 define('SITE', '');
 
 // base dir
-define("BASE_DIR", "/CourseMan");
+define("BASE_DIR", dirname(__FILE__)."/../");
 
 // absolute filesystem path to the web root
 define('WWW_DIR', dirname(__FILE__));
